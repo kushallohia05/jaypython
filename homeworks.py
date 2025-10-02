ed6155f0ff3e15643af033c20809aacac92842b3
kushallohia05 <kushallohia05@gmail.com>
2025-10-02 17:53:23 +0530

    Add files via upload

---
+++ homeworks.py
@@ -0,0 +1,15 @@
+a=input("are u playing a game? say yes/no")
+if(a=='no'):
+	pass
+a=input("is social media open? Say yes/no")
+if(a=='no'):
+	pass
+a=input("is Google open? Say yes/no")
+if(a=='no'):
+	pass
+
+a=input("all the application are closed? sy yes/no")
+if(a=='yes'):
+    print("shutdown")
+else:
+    print("not shut down")
