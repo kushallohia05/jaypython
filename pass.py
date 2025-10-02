2e800fbf52ae4c3b1351fe6159147d3db6281183
kushallohia05 <kushallohia05@gmail.com>
2025-10-02 17:01:56 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,15 @@
+for x in range(10,70):
+    if x % 20==0:
+        print("bang")
+
+    elif x % 15==0:
+        pass
+
+    elif x % 5==0:
+        print("crunch")
+        
+    elif x % 3==0:
+        print("kerching")
+    
+    else:
+        print (x)
