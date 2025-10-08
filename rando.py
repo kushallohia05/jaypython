89d24d63e808c0e1915bc01c9d38bde63af4681a
kushallohia05 <kushallohia05@gmail.com>
2025-10-08 17:33:42 +0530

    Add files via upload

---
+++ rando.py
@@ -0,0 +1,6 @@
+import math
+print("The floor and ceiling value of 23.50 are:" + str(math.ceil(23.56))+','+str(math.floor(23.56)))
+x=10
+y=-15
+print("The value of x after copying the sign from y is: "+str(math.copysign(x,y)))
+print(math.gcd(2,24))
