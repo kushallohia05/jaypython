916a055ce7b14ae7f7c420d82b1e1653d50e56c0
kushallohia05 <kushallohia05@gmail.com>
2025-10-22 17:19:46 +0530

    Add files via upload

---
+++ listapple.py
@@ -0,0 +1,11 @@
+def matchwords(words):
+    ctr=0
+    lst=[]
+    for word in words:
+        if len(word)>1 and word[0]==word[-1]:
+            ctr+=1
+            lst.append(word)
+    print("List of words with first and last character same\n", lst)
+    return ctr
+count=matchwords(['mom', 'dad', 'xyz', 'abba', '1221'])
+print("Number of words having first and last character same is", count)
