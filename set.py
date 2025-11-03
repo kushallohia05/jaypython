55e5719d4f8a881ebf85e36abc31eb345dc1e528
kushallohia05 <kushallohia05@gmail.com>
2025-11-03 17:04:56 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,16 @@
+myset={1,2,3}
+print(myset)
+
+myyset={1.0, "Hello", (1,2,3)}
+print(myyset)
+
+myset1={1,2,3,4,3,2}
+print(myset1)
+
+myyset1=set([1,2,3,2])
+print(myyset1)
+
+setty=set([0,1,3,4,5])
+print("Original set:", setty)
+setty.pop()
+print("After removing the first number from the set, the set is", setty)
