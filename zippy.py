bc2ff8f35412fbf98cd919b553a1e989be320e56
kushallohia05 <kushallohia05@gmail.com>
2025-11-05 13:14:35 +0530

    Add files via upload

---
+++ zippy.py
@@ -0,0 +1,15 @@
+s1={2,3,1}
+s2={'b', 'a', 'c'}
+s3=list(zip(s1,s2))
+print(s3)
+
+list1=[10,20,30,40]
+list2=[100,200,300,400]
+for x, y in zip(list1,list2[::-1]):
+    print(x, y)
+
+stocks=['reliance', 'infosys', 'tcs']
+prices=[2175,5000, 1111]
+newdict={stocks: prices for stocks,
+                    prices in zip(stocks, prices)}
+print('\n{}'.format(newdict))
