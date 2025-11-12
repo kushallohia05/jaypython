2d2ca3d9e7d053817c95c0f10d55503dda5842c9
kushallohia05 <kushallohia05@gmail.com>
2025-11-12 16:27:58 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+def generate_password(length=12):
+    
+    letters = string.ascii_letters  
+    digits = string.digits          
+    symbols = string.punctuation    
+    
+    all_chars = letters + digits + symbols
+    
+    
+    password = [
+        random.choice(letters),
+        random.choice(digits),
+        random.choice(symbols)
+    ]
+    
+    
+    password += random.choices(all_chars, k=length-3)
+    
+    
+    random.shuffle(password)
+    
+    
+    return ''.join(password)
+
+
+if __name__ == "__main__":
+    print("Generated Password:", generate_password(12))  
